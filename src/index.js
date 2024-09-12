02a155d3aeec92f2ad281be73816ea9cc19c94da
Raghulrv36 <raghulvenkatachalam23@gmail.com>
2024-09-13 00:45:38 +0530

    updated style.css

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function Name(props)
             <p>{props.desc} </p>
             </div>
             <div className="content">
-            <span className="time" style={{fontSize:"15px", fontWeight:"lighter"}}>11:00</span>
+            <span className="time" style={{fontSize:"15px", fontWeight:"lighter"}}>{props.time}</span>
             <span className="star">★</span>
             </div>
          </div>
@@ -41,32 +41,38 @@ var details = [
     {
         name: "Jessica Koel",
         desc: "Hey,Joel, I here to help you",
-        image: round
+        image: round,
+        time: "12:00",
     },
     {
         name: "Koemoel Bolger",
         desc: "I will send you all documents soon",
-        image: peter
+        image: peter,
+        time: "11:00",
     },
     {
         name: "Tamaara Suiel",
         desc: "Are you going to business next week",
-        image: lana
+        image: lana,
+        time: "01:00",
     },
     {
         name: "Sam Nielson",
         desc: "I Suggest you to start a new business soon",
-        image: john
+        image: john,
+        time: "08:00",
     },
     {
         name: "Caroline Nexon",
         desc: "We need you to start new research center",
-        image: photo
+        image: photo,
+        time: "03:00",
     },
     {
         name: "Patrick Koeler",
         desc: "We need you to start new research center",
-        image: jack
+        image: jack,
+        time: "07:00",
     }
 ]
 
@@ -82,7 +88,7 @@ root.render(
         {
             details.map(function(item)
         {
-            return <Name name = {item.name} desc = {item.desc} image={item.image}></Name>
+            return <Name name = {item.name} desc = {item.desc} image={item.image} time = {item.time}></Name>
         })
         }
     </div>
